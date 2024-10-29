e696658a07020e43f8f808c613a6c2b2eecb655b
jamilshehab <jamilshehab>
2024-10-29 19:17:50 +0200

    files updated

--- fibonacci.py
+++ fibonacci.py
@@ -6,13 +6,16 @@ for i in range(0,number):
      print(firstvariable) 
      previous_variable=firstvariable
      firstvariable=secondvariable
-     secondvariable=previous_variable + secondvariable        
+     secondvariable=previous_variable + secondvariable    
+
+#note I will try my best to explain the code :     
 
 #explanation of this code : first we declared a variable which stores a user input and then we declared 2 variables of values 0 and 1 
 #we looped from 0 to the user input 
 #the loop explanation we first print the first variable which is assigned by default to 0 
-#we declared a variable that stores the previous variable 
-#we updated the previous variable which is zero and assigned it into 1 
-#we assigned the second variable into previous_variable which stores the first variable which is updated into the second variable 
-#we add the second variable which is the updated variable which is 1 and then the second variable which is one as well 
-#the second time in the loop the first variable which is updated into 1 
+#we declared a variable called previous_variable that stores the first  variable  which is zero by default 
+#we updated the first variable which is zero and assigned it into the second variable which is 1 firstvariable=secondvariable
+#we apply the second variable which consist of    the previous variable stored as zero and the secondvariable which is one by default
+#we re apply the loop and the first variable becomes 1 because before updating the second variable the second variable was by default 1 so it displays 2 times in the compiler
+#the first time is by default and the second time is by 0 + 1 and the loops continue through n 
+  
